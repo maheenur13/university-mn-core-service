20488fc5a2751405dc945cbee76cfb89303681f7
maheenur13 <differentmahee13@gmail.com>
2023-08-19 18:43:19 +0600

    zod validation added

--- src/app/modules/AcademicSemester/academicSemester.routes.ts
+++ src/app/modules/AcademicSemester/academicSemester.routes.ts
@@ -1,8 +1,14 @@
 import express from 'express';
+import validateRequest from '../../middlewares/validateRequest';
 import { AcademicSemesterController } from './academicSemester.controller';
+import { academicSemesterValidation } from './academicSemester.validation';
 
 const router = express.Router();
 
-router.post('/', AcademicSemesterController.insertToDB);
+router.post(
+  '/',
+  validateRequest(academicSemesterValidation.create),
+  AcademicSemesterController.insertToDB
+);
 
 export const AcademicSemesterRoutes = router;
