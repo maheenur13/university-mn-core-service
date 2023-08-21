903b501878581b56ce12047d1d3c3444a5c5c60a
maheenur13 <differentmahee13@gmail.com>
2023-08-22 02:37:44 +0600

    academic department created

---
+++ src/app/modules/AcademicDepartment/academicDepartment.service.ts
@@ -0,0 +1,113 @@
+import { AcademicDepartemt, Prisma } from '@prisma/client';
+import { paginationHelpers } from '../../../helpers/paginationHelper';
+import { IGenericResponse } from '../../../interfaces/common';
+import { IPaginationOptions } from '../../../interfaces/pagination';
+import prisma from '../../../shared/prisma';
+import {
+  academicDepartmentRelationalFields,
+  academicDepartmentRelationalFieldsMapper,
+  academicDepartmentSearchableFields,
+} from './academicDepartment.contants';
+import { IAcademicDepartmentFilterRequest } from './academicDepartment.inderface';
+
+const insertIntoDB = async (
+  data: AcademicDepartemt
+): Promise<AcademicDepartemt> => {
+  const result = await prisma.academicDepartemt.create({
+    data,
+    include: {
+      academicFaculty: true,
+    },
+  });
+
+  return result;
+};
+
+const getAllFromDB = async (
+  filters: IAcademicDepartmentFilterRequest,
+  options: IPaginationOptions
+): Promise<IGenericResponse<AcademicDepartemt[]>> => {
+  const { limit, page, skip } = paginationHelpers.calculatePagination(options);
+  const { searchTerm, ...filterData } = filters;
+
+  const andConditions = [];
+
+  if (searchTerm) {
+    andConditions.push({
+      OR: academicDepartmentSearchableFields.map(field => ({
+        [field]: {
+          contains: searchTerm,
+          mode: 'insensitive',
+        },
+      })),
+    });
+  }
+
+  if (Object.keys(filterData).length > 0) {
+    andConditions.push({
+      AND: Object.keys(filterData).map(key => {
+        if (academicDepartmentRelationalFields.includes(key)) {
+          return {
+            [academicDepartmentRelationalFieldsMapper[key]]: {
+              id: (filterData as any)[key],
+            },
+          };
+        } else {
+          return {
+            [key]: {
+              equals: (filterData as any)[key],
+            },
+          };
+        }
+      }),
+    });
+  }
+
+  const whereConditions: Prisma.AcademicDepartemtWhereInput =
+    andConditions.length > 0 ? { AND: andConditions } : {};
+
+  const result = await prisma.academicDepartemt.findMany({
+    include: {
+      academicFaculty: true,
+    },
+    where: whereConditions,
+    skip,
+    take: limit,
+    orderBy:
+      options.sortBy && options.sortOrder
+        ? { [options.sortBy]: options.sortOrder }
+        : {
+            createdAt: 'desc',
+          },
+  });
+  const total = await prisma.academicDepartemt.count({
+    where: whereConditions,
+  });
+
+  return {
+    meta: {
+      total,
+      page,
+      limit,
+    },
+    data: result,
+  };
+};
+
+const getByIdFromDB = async (id: string): Promise<AcademicDepartemt | null> => {
+  const result = await prisma.academicDepartemt.findUnique({
+    where: {
+      id,
+    },
+    include: {
+      academicFaculty: true,
+    },
+  });
+  return result;
+};
+
+export const AcademicDepartmentService = {
+  insertIntoDB,
+  getAllFromDB,
+  getByIdFromDB,
+};
