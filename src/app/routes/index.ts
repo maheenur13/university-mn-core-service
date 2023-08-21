fd6392a45e6696b573d0166bd8c57bd0c4ca0d8b
maheenur13 <differentmahee13@gmail.com>
2023-08-22 02:39:14 +0600

    all things done and push to git

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,15 +1,35 @@
 import express from 'express';
+import { AcademicDepartmentRoutes } from '../modules/AcademicDepartment/academicDepartment.routes';
+import { academicFacultyRoutes } from '../modules/AcademicFaculty/academicFaculty.routes';
 import { AcademicSemesterRoutes } from '../modules/AcademicSemester/academicSemester.routes';
+import { facultyRoutes } from '../modules/Faculty/faculty.routes';
+import { studentRoutes } from '../modules/Student/student.routes';
 
 const router = express.Router();
 
 const moduleRoutes = [
+  // ... routes
   {
     path: '/academic-semesters',
     route: AcademicSemesterRoutes,
   },
+  {
+    path: '/academic-faculties',
+    route: academicFacultyRoutes,
+  },
+  {
+    path: '/academic-departments',
+    route: AcademicDepartmentRoutes,
+  },
+  {
+    path: '/faculties',
+    route: facultyRoutes,
+  },
+  {
+    path: '/students',
+    route: studentRoutes,
+  },
 ];
 
 moduleRoutes.forEach(route => router.use(route.path, route.route));
-
 export default router;
