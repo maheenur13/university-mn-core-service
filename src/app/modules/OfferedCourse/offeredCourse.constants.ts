140b7a1c1801b8b7e4f19193d23ddbf1a3c7bdf6
maheenur13 <differentmahee13@gmail.com>
2023-08-30 17:21:01 +0600

    offered course added

--- src/app/modules/OfferedCourse/offeredCourse.constants.ts
+++ src/app/modules/OfferedCourse/offeredCourse.constants.ts
@@ -1,2 +1,20 @@
+export const offeredCourseFilterableFields: string[] = [
+  'searchTerm',
+  'id',
+  'semesterRegistrationId',
+  'courseId',
+  'academicDepartmentId',
+];
 
-// Define your constants here
+export const offeredCourseSearchableFields: string[] = [];
+
+export const offeredCourseRelationalFields: string[] = [
+  'semesterRegistrationId',
+  'courseId',
+  'academicDepartmentId',
+];
+export const offeredCourseRelationalFieldsMapper: { [key: string]: string } = {
+  semesterRegistrationId: 'semesterRegistration',
+  courseId: 'course',
+  academicDepartmentId: 'academicDepartment',
+};
--- src/app/modules/OfferedCourse/offeredCourse.interfaces.ts
+++ src/app/modules/OfferedCourse/offeredCourse.interfaces.ts
@@ -1,2 +1,12 @@
+export type ICreateOfferedCourse = {
+  academicDepartmentId: string;
+  semesterRegistrationId: string;
+  courseIds: string[];
+};
 
-// Define your interfaces here
+export type IOfferedCourseFilterRequest = {
+  searchTerm?: string | undefined;
+  semesterRegistrationId?: string | undefined;
+  courseId?: string | undefined;
+  academicDepartmentId?: string | undefined;
+};
