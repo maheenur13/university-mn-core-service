45b88493cbe12a4bf428f9c79a207759353db546
maheenur13 <differentmahee13@gmail.com>
2023-09-14 09:44:23 +0600

    redis aded

---
+++ src/shared/redis.ts
@@ -0,0 +1,75 @@
+import { SetOptions, createClient } from 'redis';
+import config from '../config';
+import { logger } from './logger';
+
+const redisClient = createClient({
+  url: config.redis.url,
+});
+
+const redisPubClient = createClient({
+  url: config.redis.url,
+});
+
+const redisSubClient = createClient({
+  url: config.redis.url,
+});
+
+redisClient.on('error', err => logger.error('RedisError', err));
+redisClient.on('connect', () => logger.error('Redis connected'));
+
+const connect = async (): Promise<void> => {
+  await redisClient.connect();
+  await redisPubClient.connect();
+  await redisSubClient.connect();
+};
+
+const set = async (
+  key: string,
+  value: string,
+  options?: SetOptions
+): Promise<void> => {
+  await redisClient.set(key, value, options);
+};
+
+const get = async (key: string): Promise<string | null> => {
+  return await redisClient.get(key);
+};
+
+const del = async (key: string): Promise<void> => {
+  await redisClient.del(key);
+};
+
+const disconnect = async (): Promise<void> => {
+  await redisClient.quit();
+  await redisSubClient.quit();
+  await redisPubClient.quit();
+};
+
+const setAccessToken = async (userId: string, token: string): Promise<void> => {
+  const key = `access-token:${userId}`;
+  await redisClient.set(key, token, {
+    EX: Number(config.redis.tokenExpiresIn),
+  });
+};
+
+const getAccessToken = async (userId: string): Promise<string | null> => {
+  const key = `access-token:${userId}`;
+  return await redisClient.get(key);
+};
+const deleteAccessToken = async (userId: string): Promise<void> => {
+  const key = `access-token:${userId}`;
+  await redisClient.del(key);
+};
+
+export const RedisClient = {
+  set,
+  get,
+  del,
+  connect,
+  disconnect,
+  setAccessToken,
+  getAccessToken,
+  deleteAccessToken,
+  publish: redisPubClient.publish.bind(redisPubClient),
+  subscribe: redisSubClient.subscribe.bind(redisSubClient),
+};
