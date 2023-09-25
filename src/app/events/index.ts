58211f9ee34cf9d9c90d15d3269cd35cd8e15483
maheenur13 <differentmahee13@gmail.com>
2023-09-25 08:43:20 +0600

    event updated

--- src/app/events/index.ts
+++ src/app/events/index.ts
@@ -1,7 +1,9 @@
+import initFacultyEvents from '../modules/Faculty/faculty.event';
 import initStudentEvents from '../modules/Student/student.event';
 
 const subscribeToEvents = () => {
   initStudentEvents();
+  initFacultyEvents();
 };
 
 export default subscribeToEvents;
